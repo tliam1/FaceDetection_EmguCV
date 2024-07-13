2d8e914c699a24ea61970b0f273896ae25ccbaf2
tliam1 <97718694+tliam1@users.noreply.github.com>
2024-07-12 17:04:48 -1000

    Initial_Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FaceDectection
+{
+    partial class FaceScan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Scan_BTN = new System.Windows.Forms.Button();
+            this.Photo_PB = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Photo_PB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Scan_BTN
+            // 
+            this.Scan_BTN.Location = new System.Drawing.Point(782, 28);
+            this.Scan_BTN.Name = "Scan_BTN";
+            this.Scan_BTN.Size = new System.Drawing.Size(88, 23);
+            this.Scan_BTN.TabIndex = 0;
+            this.Scan_BTN.Text = "Toggle Scan";
+            this.Scan_BTN.UseVisualStyleBackColor = true;
+            this.Scan_BTN.Click += new System.EventHandler(this.Scan_BTN_Click);
+            // 
+            // Photo_PB
+            // 
+            this.Photo_PB.Location = new System.Drawing.Point(22, 28);
+            this.Photo_PB.Name = "Photo_PB";
+            this.Photo_PB.Size = new System.Drawing.Size(746, 404);
+            this.Photo_PB.TabIndex = 1;
+            this.Photo_PB.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FaceScan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 483);
+            this.Controls.Add(this.Photo_PB);
+            this.Controls.Add(this.Scan_BTN);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FaceScan";
+            this.Text = "FaceScan";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Photo_PB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Scan_BTN;
+        private System.Windows.Forms.PictureBox Photo_PB;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Dai;
+
+namespace FaceDectection
+{
+    public partial class FaceScan : Form
+    {
+        private VideoCapture cap;
+        private CascadeClassifier cascadeClassifier;
+        private bool camIsActive = false;
+        public FaceScan()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cap = new VideoCapture(0); // 0th camera (so built in webcam for me :) )
+            cap.ImageGrabbed += ProcessFrame;
+            cascadeClassifier =
+                new CascadeClassifier(Application.StartupPath +
+                                      "/haarcascade_frontalface_default.xml");
+            Console.WriteLine($"DOES CASCADE CLASSIFIER EXIST: {cascadeClassifier != null}");
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            if (!camIsActive) return;
+
+            if (InvokeRequired)
+            {
+                BeginInvoke(new Action(() => ProcessFrame(sender, e)));
+                return;
+            }
+
+            Mat frame = new Mat();
+            cap.Retrieve(frame);
+
+            if (Photo_PB.Image != null)
+            {
+                Photo_PB.Image.Dispose();
+            }
+            /*
+             * A Bitmap file displays a small dots in a pattern that,
+             * when viewed from afar, creates an overall image and that
+             * Bitmap image is a grid made of rows and columns where a
+             * specific cell is given a value that fills it in or leaves
+             * it blank thus creating an image out of the data.
+             */
+            
+            // use nextFrame to process the image
+            using (Image<Bgr, byte> nextFrame = frame.ToImage<Bgr, byte>())
+            {
+                if (nextFrame != null)
+                {
+                    var grayframe = nextFrame.Convert<Gray, byte>();
+                    var faces = cascadeClassifier.DetectMultiScale(
+                        grayframe,                          // Input image
+                        1.2,                      // Scale factor
+                        4                       // Minimum neighbor (two low and it detects multiple facial expressions on a single person)
+                    ); 
+                    Console.WriteLine(faces);
+                    //var faces = grayframe.DetectHaarCascade (DEPRECIATED) 
+                    foreach (var face in faces)
+                    {
+                        nextFrame.Draw(face, new Bgr(Color.BurlyWood), 3); //the detected face(s) is highlighted here using a box that is drawn around it/them
+                        Console.WriteLine("DRAWING BOX AROUND FACE!");
+                    }
+
+                    Photo_PB.Image = nextFrame.ToBitmap();
+                }
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*using (Image<Bgr, byte> nextFrame = cap.QueryFrame())
+            {
+
+            }*/
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (cap != null)
+            {
+                cap.Dispose();
+            }
+        }
+
+        private void Scan_BTN_Click(object sender, EventArgs e)
+        {
+            if(!camIsActive)
+                cap.Start();
+            else
+                cap.Pause();
+            camIsActive = !camIsActive;
+        }
+    }
+}
